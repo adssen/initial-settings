a89cdc4e094646d4c1770f276f2674a99462888d
Tom Choi <ddsstong@gmail.com>
2016-09-08 00:12:46 +0900

    Images show

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -5,7 +5,8 @@ class PageController < ApplicationController
   def home
   	if current_user 
         redirect_to books_path
-     end
+    end
+     @books = Book.last(4)
   end
   
   def dashboard
--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Day3
     config.active_record.raise_in_transactional_callbacks = true
     config.paperclip_defaults = { 
         storage: :s3,
+        s3_host_name: "s3.ap-northeast-2.amazonaws.com",
         s3_region: ENV['AWS_REGION'],
         s3_credentials:{
             bucket: ENV['AWS_BUCKET'],
